0b8190d517297679a040b5c79f0d27cad20cc432
uzma-niaz <uzmaniaz176@gmail.com>
2025-01-22 23:56:54 +0500

    change done

---
+++ Question#05.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int i=5,j;
+	do{
+		j=i;
+		do{
+		cout<<j;
+		j--;
+		}while(j>=1);
+	    i--;
+	    cout<<endl;
+	}while(i>=1);
+	
+	
+	
+	
+}

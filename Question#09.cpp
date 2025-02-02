8a36e9cce22671d5e7bc820e0c24369db957cfec
uzma-niaz <uzmaniaz176@gmail.com>
2025-02-02 21:24:01 +0500

    done

---
+++ Question#09.cpp
@@ -0,0 +1,35 @@
+#include <stdio.h>
+int main() {
+    int arr[100];
+    int n;
+    printf("Enter the number of elements in array: ");
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) {
+        printf("Enter element %d: ", i + 1);
+        scanf("%d", &arr[i]);
+    }
+
+    int insert;
+    printf("Enter the new value to insert into the array: ");
+    scanf("%d", &insert);
+
+    arr[n] = insert;
+    n++;
+
+    for (int i = n - 1; i > 0; i--) {
+        for (int j = 0; j < i; j++) {
+            if (arr[j] < arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        printf("%d Element in array: %d\n", i, arr[i]);
+    }
+
+    return 0;
+}
